987f417728c9c6fde7b8ff9d25eb0a6e0cfa139e
User <muso17091@gmail.com>
2025-05-10 10:21:24 +0500

    added fronted./

--- keyboards.py
+++ keyboards.py
@@ -1,6 +1,6 @@
 from aiogram.types import InlineKeyboardMarkup, InlineKeyboardButton, WebAppInfo
 
-web_app = WebAppInfo(url="https://github.com/musozorbola/p2_web_app.github.io")
+web_app = WebAppInfo(url="https://musozorbola.github.io/p2_web_app.github.io/")
 web_app_btn = InlineKeyboardMarkup(inline_keyboard=[
     [InlineKeyboardButton(text="Web App", web_app=web_app)]
 ])
